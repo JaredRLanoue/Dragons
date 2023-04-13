a126b083f545eb65afa2f6ffd54b75f6147f4653
CJKek <carterkieke@gmail.com>
2023-04-13 16:58:57 -0500

    added endpoints for groups

--- Groupify-Project/Groupify/views.py
+++ Groupify-Project/Groupify/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Group
+from .serializers import UserSerializer, GroupSerializer
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -9,4 +9,12 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
