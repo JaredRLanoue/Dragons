71ff9b1ab2926cfee191f3f98ab0a4b22d24f757
abdirahman-issack <122573769+abdirahman-issack@users.noreply.github.com>
2023-04-13 20:52:40 -0500

    Update rmvuser.py

--- Groupify-Project/Groupify/rmvuser.py
+++ Groupify-Project/Groupify/rmvuser.py
@@ -1,35 +1,29 @@
-# Need to fix this in a future backlog ticket, this is a django project while this code is
-# attempting to use flask instead.
-#
-# from flask import Flask, jsonify, request
-#
-# app = Flask(__name__)
-#
-# # assume that you have a group model with a list of group objects
-#
-# # Endpoint for removing a user from a group
-# @app.route('/groups/<int:group_id>/remove-user', methods=['POST'])
-# def remove_user_from_group(group_id):
-#     # Retrieve user ID from request body
-#     user_id = request.json.get('user_id')
-#
-#     # Retrieve group by ID from group model
-#     group = next((group for group in groups if group['id'] == group_id), None)
-#
-#     if group is None:
-#         return jsonify({'error': 'Group not found'}), 404
-#
-#     # Retrieve user by ID from the group's list of users
-#     user = next((user for user in group['users'] if user['id'] == user_id), None)
-#
-#     if user is None:
-#         return jsonify({'error': 'User not found in group'}), 404
-#
-#     # Remove user from the group's list of users
-#     group['users'].remove(user)
-#
-#     return jsonify({'message': 'User removed from group'}), 200
-#
-# # Start the server
-# if __name__ == '__main__':
-#     app.run(debug=True)
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from yourapp.models import Group
+
+@csrf_exempt
+def remove_user_from_group(request, group_id):
+    if request.method == 'POST':
+        # Retrieve user ID from request body
+        user_id = request.POST.get('user_id')
+
+        # Retrieve group by ID from group model
+        try:
+            group = Group.objects.get(pk=group_id)
+        except Group.DoesNotExist:
+            return JsonResponse({'error': 'Group not found'}, status=404)
+
+        # Retrieve user by ID from the group's list of users
+        try:
+            user = group.users.get(pk=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found in group'}, status=404)
+
+        # Remove user from the group's list of users
+        group.users.remove(user)
+
+        return JsonResponse({'message': 'User removed from group'}, status=200)
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
